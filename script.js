60d7f6383abe2a48d9680161ad4f574e9aedb6df
Umar Farooq-Ansari <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>
2023-06-09 10:36:38 -0600

    nice

---
+++ script.js
@@ -0,0 +1,11 @@
+function submit() {
+    var button = document.getElementById('btn');
+    var menu = document.getElementById('menu');
+    if(menu.style.display == 'none'){
+        menu.style.display = 'block'
+    }
+    else{
+        menu.style.display = 'none'
+    }
+
+}
